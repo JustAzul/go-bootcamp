d5f81c1708a9eff4e3ec03ecaed2966a9577a958
Azul <diego.kfu@hotmail.com>
2022-05-15 04:35:40 -0300

    lesson done

--- 3/main.go
+++ 3/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"path"
+)
 
+func main() {
+	_, file := path.Split("css/main.css")
+
+	// fmt.Println("dir: ", dir)
+	fmt.Println("file: ", file)
 }
