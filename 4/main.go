b03192d9713c490bbe8ae1eae380176f788f8b57
Azul <diego.kfu@hotmail.com>
2022-05-15 04:47:18 -0300

    lesson done

--- 4/main.go
+++ 4/main.go
@@ -1,5 +1,16 @@
 package main
 
 func main() {
+	// score := 0 // big no
+	var score int // its easy for co-workers to understand we dont know the initial value
 
+	// multiple var declaration
+	var (
+		firstVar  string
+		secondVar int
+		thirdVar  bool
+	)
+
+	// multiple var declaration with initial values
+	width, height := 100, 50
 }
